37b58163ac58bb4992ac04fe2347b332caca7258
R SHASHANK <116907094+itsshashankr14@users.noreply.github.com>
2025-06-10 22:13:17 +0530

    GFG 160 days challenge day 4

---
+++ GFG_160/left_rotate_array.py
@@ -0,0 +1,18 @@
+def left_rotate(arr, d):
+    n = len(arr)
+    d %= n
+    def reverse(start, end):
+        while start < end :
+            arr[start],arr[end] = arr[end],arr[start]
+            start += 1
+            end -= 1
+
+    reverse(0, d - 1)
+    reverse(d, n - 1)
+    reverse(n, n - 1)
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    d = 3
+    left_rotate(arr, d)
+    print("Array after left rotation:", arr)  # Output: [3, 4, 5, 6, 7, 1, 2]
